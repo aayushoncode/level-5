a64d0774e6831a6eb041c3d57db343ca04feecb5
Ayush Kumar Sinha <ayushh.dev05@gmail.com>
2026-06-30 11:40:39 +0530

    deployment

---
+++ level-5/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv, { configDotenv } from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 2000;
+
+app.get("/health", (req, res) => {
+  return res.status(200).json({
+    message: "yes you are in health route",
+  });
+});
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "hii ayush this side",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port : ${PORT}`);
+});
